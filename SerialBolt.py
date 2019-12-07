de7f76e25a202741353865b958c0eb1fc1b7122e
“pankajpatro703” <pankajkumar.p@somaiya.edu>
2019-12-07 23:47:58 +0530

    Serial communication

--- SerialBolt.py
+++ SerialBolt.py
@@ -3,5 +3,5 @@ import conf,time
 mybolt=Bolt(conf.API_KEY,conf.DEVICE_ID)
 resp=mybolt.serialBegin(9600)
 time.sleep(10)
-mybolt.serialWrite("1 2 3 \n")
+mybolt.serialWrite("89 23 83 \n")
 time.sleep(10)
