569c5a9912487924019db029182a507ed4a9f017
Pankajkumar Patro <45874442+pankajpatro703@users.noreply.github.com>
2020-02-14 21:50:53 +0530

    Added License Notice

--- src/DAS_script.py
+++ src/DAS_script.py
@@ -1,3 +1,25 @@
+'''
+    This file is part of ClimateDAS_IoT.
+    
+    ClimateDAS_IoT is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    
+    ClimateDAS_IoT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with ClimateDAS_IoT.  If not, see <https://www.gnu.org/licenses/>.
+'''
+
+################################
+#   @Author: pankajpatro703    #
+# Licensed under Lesser GPL v3 #
+################################
+
 from boltiot import Bolt
 from datetime import datetime,timedelta
 import conf,time,re
